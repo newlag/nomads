026738dc5cd7691c8a1c3a5c6de5160c057be5a3
Kamikadze986 <poma67890@gmail.com>
2023-02-18 14:56:02 +0300

    habitation

--- app/src/main/java/com/hakaton/nomads/ui/fragments/main/MainFragment.kt
+++ app/src/main/java/com/hakaton/nomads/ui/fragments/main/MainFragment.kt
@@ -10,10 +10,7 @@ import com.hakaton.nomads.data.repositories.*
 import com.hakaton.nomads.databinding.FragmentMainBinding
 import com.hakaton.nomads.domain.repositories.OrganizationsRepository
 import com.hakaton.nomads.domain.repositories.TourismRoomsRepository
-import com.hakaton.nomads.ui.fragments.main.adapters.EventMainDataClass
-import com.hakaton.nomads.ui.fragments.main.adapters.HabitationMainDataClass
-import com.hakaton.nomads.ui.fragments.main.adapters.MainFragmentRecyclerView
-import com.hakaton.nomads.ui.fragments.main.adapters.TwoTextDataClass
+import com.hakaton.nomads.ui.fragments.main.adapters.*
 import kotlinx.coroutines.flow.*
 
 class MainFragment : Fragment() {
@@ -26,13 +23,7 @@ class MainFragment : Fragment() {
     private val eventsRepository: EventsRepositoryImpl = EventsRepositoryImpl()
 
     val binding get() = _binding!!
-    val list = mutableListOf(
-        TwoTextDataClass("", ""),
-        EventMainDataClass(),
-        EventMainDataClass(),
-        EventMainDataClass(),
-        EventMainDataClass(),
-    )
+    val list = mutableListOf<SelectorMainCard>()
     val adapter = MainFragmentRecyclerView(list)
 
     override fun onCreateView(
@@ -44,20 +35,7 @@ class MainFragment : Fragment() {
         binding.recyclerView.adapter =
             adapter
 
-//        roomsRepository.getData().observe(viewLifecycleOwner) {
-//            it?.let {
-//                Log.d("MainFragment", "Получен список Rooms с размером ${it.size}")
-//            } ?: run {
-//                Log.d("MainFragment", "Ошибка получения Rooms")
-//            }
-//        }
-//        organizationsRepository.getData().observe(viewLifecycleOwner) {
-//            it?.let {
-//                Log.d("MainFragment", "Получен список Org с размером ${it.size}")
-//            } ?: run {
-//                Log.d("MainFragment", "Ошибка получения Org One")
-//            }
-//        }
+
         tourismTwoRepository.getData().shareIn(lifecycleScope, SharingStarted.Eagerly, 0).combine(
             eventsRepository.getData().shareIn(lifecycleScope, SharingStarted.Eagerly, 0)
         ) { tourism, event ->
