984c7cc8aed702d38a71423993f5933bf94cdaff
Kamikadze986 <poma67890@gmail.com>
2023-02-19 00:58:29 +0300

    profile

--- app/src/main/java/com/hakaton/nomads/ui/fragments/BaseSecondaryFragment.kt
+++ app/src/main/java/com/hakaton/nomads/ui/fragments/BaseSecondaryFragment.kt
@@ -1,14 +1,12 @@
 package com.hakaton.nomads.ui.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.hakaton.nomads.utils.ToolbarViewModel
 
-abstract class BaseSecondaryFragment : Fragment(){
+abstract class BaseSecondaryFragment : Fragment() {
     lateinit var viewModel: ToolbarViewModel
 
 
--- app/src/main/java/com/hakaton/nomads/ui/fragments/profile/ProfileFragment.kt
+++ app/src/main/java/com/hakaton/nomads/ui/fragments/profile/ProfileFragment.kt
@@ -4,18 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.hakaton.nomads.databinding.FragmentProfileBinding
+import com.hakaton.nomads.ui.fragments.BaseSecondaryFragment
 
-class ProfileFragment : Fragment() {
-    var _binding: FragmentProfileBinding? = null
-    val binding get() = _binding!!
+class ProfileFragment : BaseSecondaryFragment() {
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        binding.materialToolbar.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
         return binding.root
     }
 
